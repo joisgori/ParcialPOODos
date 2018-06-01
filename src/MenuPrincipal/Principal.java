9a7c066520bcc16ba94866ea98bb291454d7d859
Josué González <00034715@uca.edu.sv>
2018-05-31 22:56:55 -0600

    Solo he creado el paquete del menú principal con la clase contenedora del main.

---
+++ src/MenuPrincipal/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Acá colocaré el método main para la ejecución del programa/juego del siguiente proyecto - parcial dos ciclo 01/2018
+ */
+package MenuPrincipal;
+
+/**
+ *
+ * @author Josué González <00034715@uca.edu.sv>
+ */
+public class Principal {
+    
+    //Método main para la ejecución de mi videojuego.
+    public static void main(String[] args) {
+        System.out.println("Por el momento, solo imprimirá esto");
+        
+    }
+    
+}
